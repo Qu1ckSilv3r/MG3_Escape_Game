1e910fd1949e398ea582842a237fcda5c5b6216b
Julia Kott <j.kott@aol.de>
2017-05-04 16:40:12 +0200

    Forms + LED blinking

---
+++ Unity/Escape/Assets/Scripts/LEDBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LEDBehaviour : MonoBehaviour {
+
+    public GameObject led;
+    private bool flashing = false;
+    private Light ledLight;
+    private Material ledColor;
+    private int timeElapsed = 0;
+    public int flashingInterval;
+	// Use this for initialization
+	void Start () {
+        ledLight = led.GetComponent<Light>();
+        ledColor = GetComponent<MeshRenderer>().material;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        timeElapsed++;
+        if(timeElapsed == flashingInterval)
+        {
+            toggleFlashing();
+            timeElapsed = 0;
+        }
+       
+    }
+
+    public void toggleFlashing() {
+        flashing = !flashing;
+        ledLight.color = flashing ? Color.red : Color.black;
+        ledColor.color = flashing ? Color.red : new Color(225, 221, 221);
+    }
+}
