095acdad9a6b54226455e4aafc787d83b13e400d
Julia Kott <j.kott@aol.de>
2017-05-11 16:26:18 +0200

    Floorplan + Alarm

---
+++ Unity/Escape/Assets/Scripts/AlarmScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlarmScript : MonoBehaviour {
+    private AudioSource alarm;
+    private AudioSource alarmOffice;
+    private Material parentMaterial;
+    private Canvas fail;
+
+    // Use this for initialization
+    void Start () {
+        alarm = GameObject.Find("AlarmSource").GetComponent<AudioSource>();
+        alarmOffice = GameObject.Find("AlarmSourceOffice").GetComponent<AudioSource>();
+        parentMaterial = transform.parent.GetComponent<MeshRenderer>().material;
+        fail = GameObject.FindGameObjectWithTag("failed").GetComponent<Canvas>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (GameObject.FindGameObjectWithTag("MainCamera").GetComponent<RaycastScript>().wasHit == gameObject.name)
+        {
+            if (Input.GetButtonUp("Interact"))
+            {
+                if(gameObject.name == "alarm_office")
+                {
+                    fail.enabled = true;
+                    alarmOffice.Play();
+                }
+                else
+                {
+                    alarm.Play();
+                    parentMaterial.color = Color.red;
+                }
+            }
+        }
+    }
+}
