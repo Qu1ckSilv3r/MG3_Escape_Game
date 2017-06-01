3f05527fa443143776daf784e5374e8cb1164f7e
miczzz <mziesemann@gmail.com>
2017-06-01 20:41:32 +0200

    Now with radio
    Radio eingebaut, das einen Platzhalter abspielt

---
+++ Unity/Escape/Assets/Scripts/RadioPlay.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioPlay : MonoBehaviour {
+    private AudioSource audioSource;
+
+    // Use this for initialization
+    void Start () {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+
+        if (GameObject.FindGameObjectWithTag("MainCamera").GetComponent<RaycastScript>().wasHit == gameObject.name)
+        {
+            if (Input.GetButtonDown("Interact"))
+            {
+                transform.localPosition = new Vector3(transform.localPosition.x, 0.8f, transform.localPosition.z);
+                audioSource.Play();
+            }
+            if (Input.GetButtonUp("Interact"))
+            {
+                transform.localPosition = new Vector3(transform.localPosition.x, 0.81f, transform.localPosition.z);                
+            }
+        }
+    }
+    }
+
