7ef071ca97966a1ee08a6a7c3f5c3df6760c70eb
miczzz <mziesemann@gmail.com>
2017-06-19 22:53:56 +0200

    Banana Data

---
+++ Unity/Escape/Assets/Scripts/BananaData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BananaData : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void Update()
+    {
+
+        if (GameObject.FindGameObjectWithTag("MainCamera").GetComponent<RaycastScript>().wasHit == gameObject.name)
+        {
+            if (Input.GetButtonDown("Interact"))
+            {
+                Debug.Log("Data collected, game won.");
+               // transform.localPosition = new Vector3(transform.localPosition.x, 0.8f, transform.localPosition.z);
+            }
+        }
+    }
+}
